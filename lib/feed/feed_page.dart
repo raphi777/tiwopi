32a9b80114cf027da0508cf9935455dc583f0220
Raphael Feigl <raphael.feigl@gmail.com>
2021-04-08 08:19:01 +0200

    update feed

--- lib/feed/feed_page.dart
+++ lib/feed/feed_page.dart
@@ -1,8 +1,11 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:audioplayers/audio_cache.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiwopi/users/tiwopi_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 import '../home_page.dart';
 
@@ -18,29 +21,42 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage>
     with AutomaticKeepAliveClientMixin {
   TiwopiUser user = new TiwopiUser();
+  var userIdList = [];
 
   Future<Widget> _getPicture() async {
     Image image;
-    var currentUser = FirebaseAuth.instance.currentUser;
-
+    //var currentUser = FirebaseAuth.instance.currentUser;
     QuerySnapshot querySnapshot =
         await FirebaseFirestore.instance.collection('users').get();
-    var userIdList = querySnapshot.docs;
+    userIdList = querySnapshot.docs;
     print(userIdList[widget.index].id);
 
     await FirebaseFirestore.instance
         .doc('users/' + userIdList[widget.index].id)
         .get()
         .then((snapshot) {
-      //print(snapshot.data());
       user.fromMap(snapshot.data());
-      //print(user.imageFileUrls[0].toString());
       image = Image.network(
         user.imageFileUrls[0].toString(),
         fit: BoxFit.fill,
       );
     });
     return image;
+  }
+
+  _play() async {
+    /*AudioPlayer audioPlayer = AudioPlayer();
+    //int result = await audioPlayer.play(user.audioFileUrl);
+    int result = await audioPlayer.play("assets/sounds/song.mp3");
+    if (result == 1) {
+      print("something was played");
+      // success
+    }*/
+
+    AssetsAudioPlayer.newPlayer().open(
+      Audio.network(user.audioFileUrl),
+      showNotification: true,
+    );
   }
 
   @override
@@ -84,8 +100,10 @@ class _FeedPageState extends State<FeedPage>
                     return Container(
                         height: MediaQuery.of(context).size.height / 20,
                         width: MediaQuery.of(context).size.width / 10,
-                        child: CircularProgressIndicator());
-
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 50.0),
+                          child: CircularProgressIndicator(),
+                        ));
                   return Container();
                 },
               ),
@@ -107,15 +125,34 @@ class _FeedPageState extends State<FeedPage>
                         elevation: 2.0,
                         padding: EdgeInsets.all(10.0),
                         onPressed: () {
+                          int _index;
+                          if (widget.index <= userIdList.length - 2) {
+                            _index = widget.index + 1;
+                          } else {
+                            _index = 0;
+                          }
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) =>
-                                  HomePage(index: widget.index + 1),
+                              builder: (context) => HomePage(index: _index),
                             ),
                           );
                         },
                       ),
+                    ),
+                    RawMaterialButton(
+                      child: Icon(
+                        Icons.play_arrow_rounded,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      shape: CircleBorder(),
+                      fillColor: Colors.grey,
+                      elevation: 2.0,
+                      padding: EdgeInsets.all(10.0),
+                      onPressed: () {
+                        _play();
+                      },
                     ),
                     Padding(
                       padding: const EdgeInsets.only(left: 25),
